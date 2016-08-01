7b08af5e3acc0a561b2227f37f59c2d57b5f12a5
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2016-08-01 11:29:26 -0300

    Exercises 1 and 2 completed. Execise 3 in progress

---
+++ Box Shooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    // define the possible states through an enumeration
+    public enum MotionDirection
+    {
+        Spin,
+        Horizontal,
+        Vertical
+    };
+
+    // store the state
+    public MotionDirection motionState = MotionDirection.Horizontal;
+
+    // motion parameters
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    // reference to transform of the gameObject
+    private Transform m_Transform;
+
+	private void Awake ()
+    {
+        m_Transform = GetComponent<Transform>();
+	}
+
+    private void Update ()
+    {
+        switch (motionState)
+        {
+            case MotionDirection.Spin:
+                // rotate around the up axis of the gameObject
+                m_Transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+                break;
+
+            case MotionDirection.Horizontal:
+                // move left and right over time
+                m_Transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+
+            case MotionDirection.Vertical:
+                // move up and down over time
+                m_Transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+                break;
+        }
+	}
+}
